38ec6c1bcbf82c29e1a6eb35b187e28abb2bc179
Henry <aguhenrychuks@gmail.com>
2023-04-11 22:29:26 +0100

    feat: search functionality added

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 
 // stylesheet
 import "../sass/Home.css";
@@ -15,6 +15,11 @@ import image5 from "../assets/image5.svg";
 import { CiSearch } from "react-icons/ci";
 
 const Home = () => {
+  const [search, setSearch] = useState(false);
+
+  const handleSearchChange = (e) =>{
+    setSearch(true);
+  }
   return (
     <div className="home">
       <div className="logo">
@@ -22,65 +27,67 @@ const Home = () => {
       </div>
       <div className="search">
         <div className="search_box">
-          <input type="text" placeholder="Search" />
+          <input type="text" placeholder="Search" onChange={handleSearchChange}/>
           <CiSearch className="search_icon" />
         </div>
       </div>
-      <div className="result_box">
-        <div className="latest_trends">
-          <p>Latest Trends</p>
-          <div className="trends">
-            <div className="trend_card">
-              <div className="trends_image">
-                <img src={image1} alt="" />
+      {search ? (
+        <div className="result_box">
+          <div className="latest_trends">
+            <p>Latest Trends</p>
+            <div className="trends">
+              <div className="trend_card">
+                <div className="trends_image">
+                  <img src={image1} alt="" />
+                </div>
+                <div className="trends_descr">
+                  <span>Shirt with puffed sleeves</span>
+                </div>
               </div>
-              <div className="trends_descr">
-                <span>Shirt with puffed sleeves</span>
+              <div className="trend_card">
+                <div className="trends_image">
+                  <img src={image2} alt="" />
+                </div>
+                <div className="trends_descr">
+                  <span>Linen jumpsuit</span>
+                </div>
+              </div>
+              <div className="trend_card">
+                <div className="trends_image">
+                  <img src={image3} alt="" />
+                </div>
+                <div className="trends_descr">
+                  <span>White formal suit</span>
+                </div>
+              </div>
+              <div className="trend_card">
+                <div className="trends_image">
+                  <img src={image4} alt="" />
+                </div>
+                <div className="trends_descr">
+                  <span>Pattern dresses</span>
+                </div>
+              </div>
+              <div className="trend_card">
+                <div className="trends_image">
+                  <img src={image5} alt="" />
+                </div>
+                <div className="trends_descr">
+                  <span>Leather shirt dress</span>
+                </div>
               </div>
             </div>
-            <div className="trend_card">
-              <div className="trends_image">
-                <img src={image2} alt="" />
-              </div>
-              <div className="trends_descr">
-                <span>Linen jumpsuit</span>
-              </div>
+            <div className="popular">
+              <h4>Popular suggestions</h4>
+              <span>Striped shirt dress</span>
+              <span>Satin shirts</span>
+              <span>Denim jumpsuit</span>
+              <span>Leather dresses</span>
+              <span>Solid tshirts</span>
             </div>
-            <div className="trend_card">
-              <div className="trends_image">
-                <img src={image3} alt="" />
-              </div>
-              <div className="trends_descr">
-                <span>White formal suit</span>
-              </div>
-            </div>
-            <div className="trend_card">
-              <div className="trends_image">
-                <img src={image4} alt="" />
-              </div>
-              <div className="trends_descr">
-                <span>Pattern dresses</span>
-              </div>
-            </div>
-            <div className="trend_card">
-              <div className="trends_image">
-                <img src={image5} alt="" />
-              </div>
-              <div className="trends_descr">
-                <span>Leather shirt dress</span>
-              </div>
-            </div>
-          </div>
-          <div className="popular">
-            <h4>Popular suggestions</h4>
-            <span>Striped shirt dress</span>
-            <span>Satin shirts</span>
-            <span>Denim jumpsuit</span>
-            <span>Leather dresses</span>
-            <span>Solid tshirts</span>
           </div>
         </div>
-      </div>
+      ) : null}
     </div>
   );
 };
